2cd845d027ce49dabe16a658cf62ff54f2a1831a
Remano <remanovictor@icloud.coms>
2020-06-04 01:41:10 +0200

    test

--- get-establishments.php
+++ get-establishments.php
@@ -1,10 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *'); 
-<<<<<<< HEAD
-$establishmentsList = file_get_contents('./client/public/establishments.json');
-=======
-$establishmentsList = file_get_contents('establishments.json');
->>>>>>> 28ccfc8fd056e733599c02bcc039f1b73b1b940d
+$establishmentsList = file_get_contents('././client/public/establishments.json');
 
 
 header('Content-Type: application/json');
